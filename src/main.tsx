86ba1e2f9d60707ae2f2bde350d226107d737014
Guido <rialguido@gmail.com>
2023-10-16 20:50:21 -0300

    Add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from "./App"
 import "./index.css"
 import { CssBaseline, CssVarsProvider, GlobalStyles } from "@mui/joy"
 import theme from "./theme"
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/routes/NotLoggedInRoutes.tsx
+++ src/routes/NotLoggedInRoutes.tsx
@@ -7,7 +7,11 @@ import Login from "../views/Login"
 
 function NotLoggedInRoutes() {
   return createBrowserRouter(
-    createRoutesFromElements(<Route path="/login" element={<Login />}></Route>),
+    createRoutesFromElements(
+      <>
+        <Route path="/login" element={<Login />}></Route>
+      </>,
+    ),
   )
 }
 
