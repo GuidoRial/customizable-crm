d66f7a21e9c201dc26f83d0dde329debf1eded3b
Guido <rialguido@gmail.com>
2023-10-22 15:46:24 -0300

    Add extra rules

--- src/rules.ts
+++ src/rules.ts
@@ -28,3 +28,31 @@ export const mustMatch = (
       `This field must match with the ${comparator} field`,
   }
 }
+
+export const requiredNumeric = {
+  required: "This field is required",
+  pattern: {
+    value: /^-?\d+(\.\d+)?$/,
+    message: "This field must be a number",
+  },
+}
+
+export const isNotNull = (text: string) => {
+  return {
+    validate: (value: any) => value != null || `${text}`,
+  }
+}
+
+export const phonePattern = {
+  pattern: {
+    value: /^\d{3}-\d{3}-\d{4}$/i,
+    message: "Invalid phone number!",
+  },
+}
+
+export const yearPattern = {
+  pattern: {
+    value: /^\d{4}$/i,
+    message: "Invalid year format!",
+  },
+}
